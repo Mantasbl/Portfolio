cc87f045c94b4d19119b383f5b18de503d9e5fc1
Mantasbl <mantas.baltakis@gmail.com>
2019-10-18 14:14:59 +0100

    Reduced margin for mobile devices

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,7 +2,7 @@
   <nav>
     <v-row align="center" justify="center">
       <v-col cols="11" sm="10" md="10" lg="9" xl="6">
-        <v-toolbar flat color="#fafafa" class="mb-12">
+        <v-toolbar flat color="#fafafa" class="mb-md-12">
           <v-toolbar-title class=""><img :src="require('../assets/logoNamePrimary.svg')" height="20" /></v-toolbar-title>
           <v-spacer></v-spacer>
           <div class="hidden-sm-and-down">
